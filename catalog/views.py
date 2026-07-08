02a0d08a56b5559dd73bfcca96be8a88fe9bf8a6
Evgen <evgen1246@yandex.ru>
2026-07-08 18:31:23 +0300

    Работа над шаблонизацией.

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.select_related("category").all()
+    context = {
+        "products": products,
+    }
+    return render(request, "home.html", context)
 
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        "product": product,
+    }
+    return render(request, "product_detail.html", context)
