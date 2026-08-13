d508bf953d89a4828508ac59c8e4d8378f735f11
Evgen <evgen1246@yandex.ru>
2026-08-13 17:55:57 +0300

    Настройка кэширования и сервисный модуль

--- catalog/views.py
+++ catalog/views.py
@@ -7,6 +7,7 @@ from django.views.generic import CreateView, DeleteView, DetailView, ListView, T
 
 from .forms import ProductForm
 from .models import Product
+from .services import get_products_by_category, get_category_by_id, get_all_categories
 
 
 class ProductListView(ListView):
@@ -148,3 +149,23 @@ class ProductUnpublishView(LoginRequiredMixin, PermissionRequiredMixin, DetailVi
             messages.error(self.request, "У вас нет прав для отмены публикации этого товара.")
             raise PermissionDenied
         return product
+
+
+class ProductsByCategoryView(ListView):
+    """
+    Представление для отображения продуктов в указанной категории.
+    """
+    template_name = 'catalog/products_by_category.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        return get_products_by_category(category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_category_by_id(category_id)
+        context['category'] = category
+        context['categories'] = get_all_categories()
+        return context
